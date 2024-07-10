b45db070bff20ede1075aad9d8db5d91f04f033b
ThomasYagi <thomas20082001@gmail.com>
2024-07-10 16:51:36 +0700

    ver-2.0

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st 
+from PIL import Image
+import numpy as np
+from decrypt import decryptPage
+from encrypt import encryptPage
+
+st.set_page_config(page_title="LSB Stego App", page_icon=":eyes:", layout="wide")
+
+# Set up the Streamlit app
+st.title('Tugas Akhir')
+st.header('Steganografi LSB pada Sampul Buku')
+
+st.write("---")
+
+PAGES = {
+    "Enkripsi" : encryptPage,
+    "Dekripsi": decryptPage,
+}
+
+st.sidebar.title("Menu")
+selection = st.sidebar.radio("Pilih menu", list(PAGES.keys()))
+
+page = PAGES[selection]
+page()
